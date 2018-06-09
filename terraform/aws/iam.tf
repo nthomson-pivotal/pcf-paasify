356464fd96f35af9ac177987d7891008f6d03211
Niall Thomson <nthomson@pivotal.io>
2018-06-08 22:53:37 -0700

    Added IAM file

---
+++ terraform/aws/iam.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_access_key" "key" {
+  user = "${aws_iam_user.user.name}"
+}
+
+resource "aws_iam_user" "user" {
+  name = "paasify-${var.env_name}"
+  path = "/paasify/"
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name = "paasify-${var.env_name}"
+  user = "${aws_iam_user.user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
