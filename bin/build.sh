ef3eeb57508e91473d5d61dd5f85591963ef7905
Niall Thomson <nthomson@pivotal.io>
2018-06-12 16:00:38 -0700

    Pulled out some commands to build.sh

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tf_state=$HOME/state/terraform.tfstate
+
+TF_VAR_env_name=$env TF_VAR_dns_suffix=$dns_suffix TF_VAR_pivnet_token=$pivnet_token TF_VAR_ssl_cert_path=$HOME/lego/certificates/pcf.$env.$dns_suffix.crt \
+  TF_VAR_ssl_private_key_path=$HOME/lego/certificates/pcf.$env.$dns_suffix.key terraform apply -input=false -auto-approve \
+  -state=$tfstate $CODEBUILD_SRC_DIR/terraform/aws
+
+export OM_TARGET=https://$(terraform output -state=$tfstate opsman_host)
+export OM_USER=$(terraform output -state=$tfstate opsman_user)
+export OM_PASSWORD=$(terraform output -state=$tfstate opsman_password)
+
+om apply-changes
