7d39c72ede84801918033926045f22ee03cdaf0a
Niall Thomson <nthomson@pivotal.io>
2018-07-31 07:32:00 -0700

    Pulled out version numbers to constants when installing tools on OpsMan

--- terraform/common/scripts/provision_opsman.sh
+++ terraform/common/scripts/provision_opsman.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+PIVNET_CLI_VERSION=0.0.52
+OM_VERSION=0.34.0
+
 pivnet_api_token=$1
 
 # Install and configure dnsmasq so our fake domains work
@@ -17,7 +20,7 @@ sudo apt-get -qq install -y jq
 # Install pivnet CLI
 echo 'Downloading pivnet CLI...'
 
-wget -q https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.49/pivnet-linux-amd64-0.0.49 -O pivnet
+wget -q https://github.com/pivotal-cf/pivnet-cli/releases/download/v$PIVNET_CLI_VERSION/pivnet-linux-amd64-$PIVNET_CLI_VERSION -O pivnet
 
 sudo mv pivnet /usr/bin
 sudo chmod +x /usr/bin/pivnet
@@ -27,7 +30,7 @@ echo 'Installed pivnet CLI'
 # Install om CLI
 echo 'Downloading om CLI...'
 
-wget -q https://github.com/pivotal-cf/om/releases/download/0.34.0/om-linux -O om
+wget -q https://github.com/pivotal-cf/om/releases/download/$OM_VERSION/om-linux -O om
 
 sudo mv om /usr/bin
 sudo chmod +x /usr/bin/om
