a5e747b88fbc7ef9a1260943de60e2eb728d1cf2
Niall Thomson <nthomson@pivotal.io>
2018-08-28 13:23:42 -0700

    Changed plan to apply for real test

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,5 @@ tf_state=$HOME/state/terraform.tfstate
 
 export TF_VAR_tiles="[$tiles]"
 
-echo $TF_VAR_tiles
-
 TF_VAR_env_name=$env TF_VAR_dns_suffix=$dns_suffix TF_VAR_pivnet_token=$pivnet_token \
-  terraform plan -input=false -state=$tf_state $CODEBUILD_SRC_DIR/terraform/aws
+  terraform apply -input=false -state=$tf_state $CODEBUILD_SRC_DIR/terraform/aws
