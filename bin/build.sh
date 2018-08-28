33f18847a60b40589a62b83d5048741fc41405e0
Niall Thomson <nthomson@pivotal.io>
2018-08-28 13:19:03 -0700

    Testing wiring custom tile list in via parameters

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,9 @@ set -e
 
 tf_state=$HOME/state/terraform.tfstate
 
-tiles_env="'[$tiles]'"
+export TF_VAR_tiles="'[$tiles]'"
 
-echo $tiles_env
+echo $TF_VAR_tiles
 
-TF_VAR_env_name=$env TF_VAR_dns_suffix=$dns_suffix TF_VAR_pivnet_token=$pivnet_token TF_VAR_tiles=$tiles_env \
+TF_VAR_env_name=$env TF_VAR_dns_suffix=$dns_suffix TF_VAR_pivnet_token=$pivnet_token \
   terraform plan -input=false -state=$tf_state $CODEBUILD_SRC_DIR/terraform/aws
