a171cada5e337c4c04dcbed1b9a3144fab2f3fbc
Niall Thomson <nthomson@pivotal.io>
2018-08-29 10:17:08 -0700

    Export OM auth env variables

--- terraform/common/scripts/install_prometheus_dev.sh
+++ terraform/common/scripts/install_prometheus_dev.sh
@@ -7,6 +7,9 @@ om_password=$2
 
 filename="prometheus-0.1.0.pivotal"
 
+export OM_USERNAME=$om_username
+export OM_PASSWORD=$om_password
+
 if [ ! -f "$filename" ]; then
   echo "Downloading Prometheus Dev tile..."
   wget https://storage.googleapis.com/prometheus-tile-dev/$filename
