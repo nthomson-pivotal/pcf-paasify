e72271d3d293192a58e93c8d5271f8ef2ccf77ab
Niall Thomson <nthomson@pivotal.io>
2018-08-29 17:49:38 -0700

    Testing custom flow for gcp in codebuild

--- bin/cloud.sh
+++ bin/cloud.sh
@@ -16,11 +16,9 @@ if [ "$cloud" = "gcp" ]; then
     echo "Bootstrapping Google Cloud..."
 
     #export GCLOUD_ACCOUNT=$(aws ssm get-parameter --name /paasify/gcp/account_name | jq '.Parameter.Value' -r)
-    export GCLOUD_PROJECT=$(aws ssm get-parameter --name /paasify/gcp/project_name | jq '.Parameter.Value' -r)
+    export TF_VAR_project=$(aws ssm get-parameter --name /paasify/gcp/project_name | jq '.Parameter.Value' -r)
 
     aws ssm get-parameter --name /paasify/gcp/auth.json --with-decryption | jq '.Parameter.Value' -r > /tmp/auth.json
 
     export GOOGLE_APPLICATION_CREDENTIALS=/tmp/auth.json
-
-    export TF_VAR_project=$GCLOUD_PROJECT_NAME
 fi
