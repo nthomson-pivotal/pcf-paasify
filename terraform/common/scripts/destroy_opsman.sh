9d6640caecad6f53cd98ada4e6cea14f66334506
Niall Thomson <nthomson@pivotal.io>
2018-09-11 23:16:03 +0100

    Reverting: Commenting out line until bug fixed

--- terraform/common/scripts/destroy_opsman.sh
+++ terraform/common/scripts/destroy_opsman.sh
@@ -2,6 +2,8 @@
 
 # This script unstages all products from OpsManager and runs `apply-changes` to make sure IaaS resources are cleaned up
 
+# TODO: If theres nothing to remove then don't apply changes, it will fail
+
 om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD --format json staged-products | jq -r '. as $orig | length as $len | [keys[] | $orig[$len - 1 - .]] | .[].name' | \
   while read object; do 
     om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD unstage-product -p $object
@@ -9,4 +11,4 @@ om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD --format json staged
 
 sleep 10
 
-#om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD apply-changes
+om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD apply-changes
