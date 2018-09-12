a8260a5e6f626b8b28df140ae796648bd8676adf
Niall Thomson <nthomson@pivotal.io>
2018-09-12 12:30:05 +0100

    Try using main service account for file storage in GCP due to permissions errors

--- terraform/gcp/pas.tf
+++ terraform/gcp/pas.tf
@@ -3,8 +3,8 @@ data "template_file" "pas_product_configuration" {
 
   vars {
     project    = "${module.gcp.pas_blobstore_service_account_project}"
-    service_account_email = "${module.gcp.pas_blobstore_service_account_email}"
-    service_account_key    = "${jsonencode(module.gcp.pas_blobstore_gcp_service_account_key)}"
+    service_account_email = "${base64decode(google_service_account.account.email)}"
+    service_account_key    = "${jsonencode(base64decode(google_service_account_key.key.private_key))}"
     droplets_bucket = "${module.gcp.droplets_bucket}"
     packages_bucket = "${module.gcp.packages_bucket}"
     buildpacks_bucket = "${module.gcp.buildpacks_bucket}"
