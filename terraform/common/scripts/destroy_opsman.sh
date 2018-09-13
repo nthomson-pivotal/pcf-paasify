24a601d567c7ceaf713fedf01e107b294612fd5b
Niall Thomson <nthomson@pivotal.io>
2018-09-13 11:55:10 +0100

    Added logic to skip apply changes on destroy if there are no products to unstage

--- terraform/common/scripts/destroy_opsman.sh
+++ terraform/common/scripts/destroy_opsman.sh
@@ -4,11 +4,19 @@
 
 # TODO: If theres nothing to remove then don't apply changes, it will fail
 
+counter=0
+
 om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD --format json staged-products | jq -r '. as $orig | length as $len | [keys[] | $orig[$len - 1 - .]] | .[].name' | \
   while read object; do 
     om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD unstage-product -p $object
+    counter=$((counter+1))
   done
 
 sleep 10
 
-om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD apply-changes
+# Only apply changes if we unstaged at least one product
+if [ "$counter" -gt "0" ]; then
+  om -k -t https://$OM_DOMAIN -u $OM_USERNAME -p $OM_PASSWORD apply-changes
+else
+  echo "Skipping apply changes..."
+fi
