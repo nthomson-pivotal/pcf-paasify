bed007bafe41a4983e13e5687e3d29e4c00c6b7d
Niall Thomson <nthomson@pivotal.io>
2018-09-13 18:46:19 +0100

    Added flag to allow disabling the scripts automatically applying changes in OpsMan once tiles are installed

--- bin/build.sh
+++ bin/build.sh
@@ -10,6 +10,7 @@ export TF_VAR_env_name=$env
 export TF_VAR_dns_suffix=$dns_suffix 
 export TF_VAR_pivnet_token=$pivnet_token
 export TF_VAR_tiles="[$tiles]"
+export TF_VAR_auto_apply=$auto_apply
 
 if [ "$command" = "plan" ]; then
     terraform plan -state=$tf_state $CLOUD_TF_DIR
