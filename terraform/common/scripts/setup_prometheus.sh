df19af9eaf005a88a2841df05767b2283e0d9ca4
Niall Thomson <nthomson@pivotal.io>
2018-10-18 11:46:32 -0700

    Removed echo from prometheus install script

--- terraform/common/scripts/setup_prometheus.sh
+++ terraform/common/scripts/setup_prometheus.sh
@@ -4,5 +4,4 @@ set -e
 
 om -k -t https://$OM_DOMAIN stage-product -p prometheus-dev -v 0.1.0
 
-echo 'Configuring Prometheus...'
 om -t https://$OM_DOMAIN configure-product --product-name prometheus-dev -pn "$AZ_CONFIG" -pr "$RESOURCE_CONFIG"
