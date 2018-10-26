a58bb68bacd63fbbb5479797dbfc73580d2c9bde
Niall Thomson <nthomson@pivotal.io>
2018-10-25 18:36:17 -0700

    Delete destroy.sh

--- terraform/common/destroy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-terraform destroy -var 'env_name=niall' -var 'project=fe-nthomson' -var 'pivnet_token=9d5sppPU4zLzCgXB_k3V' -var 'root_dns_zone=pcf-zone' -input=false -auto-approve
