d1cc9885bbd1d882a426883bd596bf26256e16ac
Niall Thomson <nthomson@pivotal.io>
2019-06-04 12:42:55 -0700

    Reworking codebuild for pipelines

--- codebuild/bin/destroy.sh
+++ codebuild/bin/destroy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-. $CODEBUILD_SRC_DIR/bin/cloud.sh
+. $CODEBUILD_SRC_DIR/codebuild/bin/cloud.sh
 
 TF_WARN_OUTPUT_ERRORS=1 TF_VAR_env_name=$env TF_VAR_dns_suffix=$dns_suffix TF_VAR_pivnet_token=$pivnet_token \
  terraform destroy -input=false -auto-approve -state=$HOME/state/terraform.tfstate $CLOUD_TF_DIR
--- codebuild/bin/install.sh
+++ codebuild/bin/install.sh
@@ -3,12 +3,11 @@
 set -e
 
 TERRAFORM_VERSION=0.11.7
-OM_CLI_VERSION=0.37.0
 
 # Dependencies
 wget -q -O terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 sudo unzip terraform.zip -d /bin && rm terraform.zip
-sudo wget -q -O /bin/om https://github.com/pivotal-cf/om/releases/download/$OM_CLI_VERSION/om-linux && chmod +x /bin/om
+
 apt-get update
 apt-get install -qq -y curl jq
 
@@ -16,7 +15,7 @@ apt-get install -qq -y curl jq
 mkdir $HOME/state
 aws s3 cp s3://$state_bucket $HOME/state --recursive
 
-. $CODEBUILD_SRC_DIR/bin/cloud.sh
+. $CODEBUILD_SRC_DIR/codebuild/bin/cloud.sh
 
 # Init Terraform
 terraform init -input=false $CLOUD_TF_DIR
