85558201b72a568a67361829c3c952beb0cf43fb
Niall Thomson <nthomson@pivotal.io>
2019-06-05 19:07:24 -0700

    Added some more logging to cloud codebuild script

--- codebuild/bin/cloud.sh
+++ codebuild/bin/cloud.sh
@@ -29,4 +29,6 @@ if [ "$cloud" = "azure" ]; then
     export ARM_CLIENT_SECRET=$(aws ssm get-parameter --name /paasify/azure/client_secret --with-decryption | jq '.Parameter.Value' -r)
     export ARM_SUBSCRIPTION_ID=$(aws ssm get-parameter --name /paasify/azure/subscription_id | jq '.Parameter.Value' -r)
     export ARM_TENANT_ID=$(aws ssm get-parameter --name /paasify/azure/tenant_id | jq '.Parameter.Value' -r)
+
+    echo "Authenticating to subscription $ARM_SUBSCRIPTION_ID with client ID $ARM_CLIENT_ID"
 fi
