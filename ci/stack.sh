1f7be4e1dc4bb5f4651fa4f53d52afb08f5effba
Niall Thomson <nthomson@pivotal.io>
2019-06-12 21:40:51 -0700

    Pull pivnet token from SSM in CI

--- ci/stack.sh
+++ ci/stack.sh
@@ -4,8 +4,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-pivnet_token=$1
-cloud=$2
+cloud=$1
 
 if [ -z "$pivnet_token" ]; then
   echo "Error: Missing Pivnet token"
@@ -17,7 +16,6 @@ if [ -z "$cloud" ]; then
   exit 1
 fi
 
-
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 env_name="ci-${branch//./-}"
@@ -26,4 +24,4 @@ stack_name="paasify-${env_name}-${cloud}"
 echo "Updating stack $stack_name for environment $env_name on $cloud"
 
 # aws cli doens't seem to like when this is formatted on to multiple lines, bleh
-aws cloudformation create-stack --stack-name $stack_name --capabilities CAPABILITY_NAMED_IAM --template-body file://$DIR/../codebuild/cloudformation/cf.json --parameters ParameterKey=EnvName,ParameterValue=$env_name ParameterKey=DnsSuffix,ParameterValue=$cloud.paasify.org ParameterKey=PivnetToken,ParameterValue=$pivnet_token ParameterKey=Cloud,ParameterValue=$cloud ParameterKey=SourceBranch,ParameterValue=$branch
+aws cloudformation create-stack --stack-name $stack_name --capabilities CAPABILITY_NAMED_IAM --template-body file://$DIR/../codebuild/cloudformation/cf.json --parameters ParameterKey=EnvName,ParameterValue=$env_name ParameterKey=DnsSuffix,ParameterValue=$cloud.paasify.org ParameterKey=Cloud,ParameterValue=$cloud ParameterKey=SourceBranch,ParameterValue=$branch
